249edc93e1d047c47c9a099e0d63c4465e29caff
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-07-04 23:47:21 +0300

    added the mutation part

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,10 +1,22 @@
-import SingleItem from './SingleItem';
+import { useQuery } from "@tanstack/react-query";
+import SingleItem from "./SingleItem";
+import customFetch from "./utils";
+
 const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ["tasks"],
+    queryFn: () => customFetch.get("/"),
+  });
+
+  if (result.isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading....</p>;
+  }
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {result.data.data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
+     
     </div>
   );
 };
