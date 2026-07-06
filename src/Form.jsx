3bcf1fbf601aaccd0c57a40178342a48b748f7f4
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-07-06 21:19:44 +0300

    added the error handling

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,13 +1,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import customFetch from "./utils";
+import { toast } from "react-toastify";
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState("");
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
 
   const { mutate: createTask, isLoading } = useMutation({
     mutationFn: (taskTitle) => customFetch.post("/", { title: taskTitle }),
+    onSuccess: () => {},
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
   });
 
   // console.log(result);
